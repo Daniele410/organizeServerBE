b862c4b5101a17cd8f1932a3c3054e922adc18f8
Daniele410 <miraglia.daniele410@gmail.com>
2024-09-11 11:50:45 +0200

    first commit,add model

---
+++ organizeserver/src/main/java/com/danozzo/organizeserver/OrganizeserverApplication.java
@@ -0,0 +1,13 @@
+package com.danozzo.organizeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrganizeserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrganizeserverApplication.class, args);
+	}
+
+}
---
+++ organizeserver/src/test/java/com/danozzo/organizeserver/OrganizeserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danozzo.organizeserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizeserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
